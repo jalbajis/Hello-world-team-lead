83c2a7028935aaaaa32c9eeaa2e4e7a0c054c42b
jalbajis <jalbaji122@gmail.com>
2022-01-22 09:08:08 -0800

    this is he 2nd commit to branchx

--- src/hello/hello2.java
+++ src/hello/hello2.java
@@ -4,7 +4,7 @@ public class hello2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Mr");
+System.out.println("Hello Jalbaji sakhare");
 		
 		
 		
